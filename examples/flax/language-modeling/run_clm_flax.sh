990b7d262ee403c530491d071676e2abfedfaad0
Matthew Finlayson <matthewf@allenai.org>
2022-06-07 15:57:38 -0700

    asdfa

--- examples/flax/language-modeling/run_clm_flax.sh
+++ examples/flax/language-modeling/run_clm_flax.sh
@@ -1,9 +1,9 @@
 python run_clm_flax.py \
   --model_name_or_path EleutherAI/gpt-neo-125M \
-  --output_dir del/ \
+  --output_dir test-repo/ \
   --dataset_name oscar \
   --dataset_config_name="unshuffled_deduplicated_no" \
   --do_train \
   --overwrite_output_dir \
-  --freeze_wte
-
+  --push_to_hub \
+  --freeze_wte \
